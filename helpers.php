1f9064cfdbdf13491b72e4479da1de608835a97a
wisdom ebuka <w>
2026-04-07 14:36:44 +0100

    Update helpers, index, home view and add new partials

--- helpers.php
+++ helpers.php
@@ -8,3 +8,51 @@
 function basePath($path = ''):string{
     return __DIR__ .'/'. $path;
 }
+
+
+/**
+ * load a view
+ * @param string $name
+ * @return void
+ */
+
+function loadViews($name):void{
+
+  $viewPath = basePath("views/{$name}.views.php");
+
+   if(!file_exists($viewPath)){
+     echo "views {$name} not found!";
+   }
+
+   require $viewPath;
+
+   
+}
+
+
+/**
+ * load a partials
+ * @param string $name
+ * @return void
+ */
+
+function loadPartials($name):void{
+    $partialPath =  basePath("views/partials/{$name}.php");
+
+    if(!file_exists($partialPath)){
+       echo "partial {$name} not found";
+    }
+
+     require $partialPath;
+}
+
+/**
+ * inspect a value(s)
+ * @param mixed $value
+ * @return void
+ */
+
+
+function inspect($value){
+
+}
